b9558d7e72cf4f54ca539cdcac70a43cdd2d22d3
zengc <zengchao405@163.com>
2024-05-20 21:36:18 +0800

    feat(springboot 集成flowable UI 6.4.2): springboot 集成flowable UI 6.4.2
    springboot 集成flowable UI 6.4.2

---
+++ src/main/java/com/zc/flowable/SpringbootFlowableUiV64Application.java
@@ -0,0 +1,13 @@
+package com.zc.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFlowableUiV64Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFlowableUiV64Application.class, args);
+    }
+
+}
